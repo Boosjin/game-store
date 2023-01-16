4c4e547cc313286a863a3004efb985260eaca0fb
Kourosh Khosravi Boosjin <kourosh.khosravi.boosjin@gmail.com>
2023-01-16 19:43:35 +0330

    add a new constraint

--- src/main/java/website/kourosh/gamestore/videogame/version/Version.java
+++ src/main/java/website/kourosh/gamestore/videogame/version/Version.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,5 +28,6 @@ public final class Version {
 
     @Column
     @NotNull
+    @PastOrPresent
     private LocalDate releaseDate;
 }
