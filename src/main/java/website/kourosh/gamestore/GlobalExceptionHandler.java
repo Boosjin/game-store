917c732cdaab0ab44808cc66b69ccc16d408da56
Kourosh Khosravi Boosjin <kourosh.khosravi.boosjin@gmail.com>
2023-01-19 04:21:34 +0330

    change exception type

--- src/main/java/website/kourosh/gamestore/GlobalExceptionHandler.java
+++ src/main/java/website/kourosh/gamestore/GlobalExceptionHandler.java
@@ -1,19 +1,22 @@
 package website.kourosh.gamestore;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.LocalDateTime;
-
 @ControllerAdvice
 class GlobalExceptionHandler {
 
     @ExceptionHandler
-    private ResponseEntity<String> globalExceptionHandler(RuntimeException runtimeException) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(LocalDateTime.now() + "\n\n" + runtimeException.getMessage());
+    private ResponseEntity<String> constraintViolationException(ConstraintViolationException e) {
+
+        StringBuilder violations = new StringBuilder("Constraint Violation(s)\n\n");
+        e.getConstraintViolations().forEach(constraintViolation -> violations.append(constraintViolation.getMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(violations.toString());
     }
 
     // TODO add more specific exception handlers
